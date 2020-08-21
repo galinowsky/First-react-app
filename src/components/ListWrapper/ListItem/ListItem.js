d73b2c87c1b883cc3c81cd336ac4e961fc7eac9e
Jakub Galinski <thougless@gmail.com>
2020-08-21 17:18:48 +0200

    created Title and Button components

--- src/components/ListWrapper/ListItem/ListItem.js
+++ src/components/ListWrapper/ListItem/ListItem.js
@@ -1,6 +1,8 @@
 import React from "react";
 import styles from "./ListItem.module.scss";
 import PropTypes from "prop-types";
+import Button from "../../Button/Button";
+import Title from "../../Title/Title";
 const ListItem = ({ image, name, description, twitterLink }) => {
   const ImageTag = image ? "img" : "div";
   return (
@@ -11,16 +13,16 @@ const ListItem = ({ image, name, description, twitterLink }) => {
         alt={name}
       />
       <div>
-        <h2 className={styles.name}>{name}</h2>
+        <Title className={styles.name}>{name}</Title>
         <p className={styles.description}>{description}</p>
-        <a
+        <Button
           href={twitterLink}
           target="_blank"
           className={styles.button}
           rel="noopener noreferrer"
         >
           visit twitter page
-        </a>
+        </Button>
       </div>
     </li>
   );
